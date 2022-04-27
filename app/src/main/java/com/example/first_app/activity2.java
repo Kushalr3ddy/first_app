e2b11df8f3eeeb30aa22eb35be21b02744742cc2
kushalr3ddy <pikkikushal@gmail.com>
2022-04-27 12:17:14 +0530

    half home page

--- app/src/main/java/com/example/first_app/activity2.java
+++ app/src/main/java/com/example/first_app/activity2.java
@@ -4,10 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class activity2 extends AppCompatActivity {
@@ -17,34 +21,36 @@ public class activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
-        //getSupportActionBar().setTitle("covid tracking app");
+        Button self_asses = (Button)findViewById(R.id.button1);
+        Button vaccinate = (Button)findViewById(R.id.button2);
+        TextView terms = (TextView)findViewById(R.id.terms);
+
+        self_asses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent next = new Intent(activity2.this,self_assessment.class);
+                startActivity(next);
+            }
+        });
+        vaccinate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://selfregistration.cowin.gov.in/"));
+                startActivity(browserIntent);
+            }
+        });
+
+        terms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent show_terms = new Intent(activity2.this,terms_page.class);
+                startActivity(show_terms);
+
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.example_menu,menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        Intent intent1 = new Intent(activity2.this,MainActivity.class);
 
-        switch(item.getItemId()){
-            case R.id.item1:
-                Toast.makeText(this,"logging out",Toast.LENGTH_SHORT).show();
-                startActivity(intent1);
-                return true;
-            case R.id.item2:
-                Toast.makeText(this,"logging out",Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.item3:
-                Toast.makeText(this,"logging out",Toast.LENGTH_SHORT).show();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
 
-    }
 }
